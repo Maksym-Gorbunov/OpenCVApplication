43ada99d968260552e7659493d6c86382ae4e684
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-07-07 20:02:12 +0200

    ImageLabel->loadImage

--- src/com/recognition/gui/ImagePanel.java
+++ src/com/recognition/gui/ImagePanel.java
@@ -1,9 +1,10 @@
 package com.recognition.gui;
 
 import com.recognition.constants.Constants;
-
 import javax.swing.*;
 import java.awt.BorderLayout;
+import java.awt.Image;
+import java.io.File;
 
 public class ImagePanel extends JPanel{
   private static final long serialVersionUID = 1L;
@@ -11,10 +12,23 @@ public class ImagePanel extends JPanel{
 
   public ImagePanel(){
     this.imageLabel = new JLabel();
-
     setLayout(new BorderLayout());
     setBorder(BorderFactory.createEmptyBorder(Constants.IMAGE_LABEL_BORDER, Constants.IMAGE_LABEL_BORDER,
             Constants.IMAGE_LABEL_BORDER, Constants.IMAGE_LABEL_BORDER));
     add(imageLabel, BorderLayout.CENTER);
+  }
+
+  public void updadeImage(final Image image) {
+    imageLabel.setIcon(new ImageIcon(scaleImage(image)));
+  }
+
+  private Image scaleImage(Image image) {
+    return image.getScaledInstance(700, 500, Image.SCALE_SMOOTH);
+  }
+
+  public void loadImage(File file) {
+    this.transformedImageIcon = new ImageIcon(file.getAbsolutePath());
+    Image image = transformedImageIcon.getImage();
+    updadeImage(image);
   }
 }
