dbde1a84b66a9f1a5d6614ccfc7cf8363cf17a6d
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-07-07 15:14:47 +0200

    constants

---
+++ src/com/recognition/app/App.java
@@ -0,0 +1,23 @@
+package com.recognition.app;
+
+import com.recognition.gui.MainFrame;
+
+import javax.swing.*;
+
+public class App {
+  public static void main(String[] args) {
+    try{
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (ClassNotFoundException | InstantiationException
+            | UnsupportedLookAndFeelException | IllegalAccessException e) {
+      e.printStackTrace();
+    }
+
+    SwingUtilities.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        new MainFrame();
+      }
+    });
+  }
+}
