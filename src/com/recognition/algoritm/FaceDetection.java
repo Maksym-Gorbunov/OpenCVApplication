6f1cc1231a2f30cfe30f8b9331dfd66ac440f447
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-07-07 21:23:36 +0200

    Face Detection update image

--- src/com/recognition/algoritm/FaceDetection.java
+++ src/com/recognition/algoritm/FaceDetection.java
@@ -1,8 +1,15 @@
 package com.recognition.algoritm;
 
 import com.recognition.constants.Constants;
-import org.opencv.core.Core;
+import com.recognition.gui.ImagePanel;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
+
+import java.awt.image.BufferedImage;
+
+import java.io.File;
 
 public class FaceDetection {
   private CascadeClassifier cascadeClassifier;
@@ -10,5 +17,26 @@ public class FaceDetection {
   public FaceDetection() {
     System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     this.cascadeClassifier = new CascadeClassifier(Constants.CASCADE_CLASSIFIER);
+  }
+
+  public void detectFaces(File file, ImagePanel imagePanel) {
+    Mat image = Imgcodecs.imread(file.getAbsolutePath(), Imgcodecs.IMREAD_COLOR);
+
+    MatOfRect faceDetections = new MatOfRect();
+    cascadeClassifier.detectMultiScale(image, faceDetections);
+
+    // Paint rectangles around recognized faces
+    for (Rect rect : faceDetections.toArray()) {
+      Imgproc.rectangle(image,
+              new Point(rect.x, rect.y),
+              new Point(rect.x + rect.width, rect.y + rect.height),
+              new Scalar(100, 100, 250), 10);
+    }
+
+    BufferedImage bufferedImage = convertMatToImage(image);
+    imagePanel.updadeImage(bufferedImage);
+  }
+
+  private BufferedImage convertMatToImage(Mat image) {
   }
 }
--- src/com/recognition/constants/Constants.java
+++ src/com/recognition/constants/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
   public static final int FRAME_WIDTH = 800;
   public static final int FRAME_HEIGTH = 600;
   public static final int IMAGE_LABEL_BORDER = 30;
-  public static final String CASCADE_CLASSIFIER = "C:\\java\\OpenCVApplication\\images\\";
+  public static final String projectPath = "c:\\java\\OpenCVApplication";
+  public static final String CASCADE_CLASSIFIER = projectPath + "\\lib\\haarcascade_frontalface_alt.xml";
 
 }
