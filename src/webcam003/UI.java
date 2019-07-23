4962114ec36e7a042e4bdae54091682a0632d0e4
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-07-23 12:26:40 +0200

    ui002

--- src/webcam003/UI.java
+++ src/webcam003/UI.java
@@ -1,6 +1,8 @@
 package webcam003;
 
+import com.recognition.image.algoritm.FaceDetection;
 import com.recognition.image.constants.Constants;
+import com.recognition.image.gui.ImagePanel;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,20 +13,52 @@ public class UI extends JFrame {
   private JButton startBtn;
   private JButton pauseBtn;
 
+  private ImagePanel imagePanel;
+  private JFileChooser fileChooser;
+
 
   public UI() {
     super(Constants.APPLICATION_NAME);
+    setJMenuBar(createMenuBar());
 
     this.panel = new JPanel();
     this.startBtn = new JButton();
     this.pauseBtn = new JButton();
+
+    this.imagePanel = new ImagePanel();
+    this.fileChooser = new JFileChooser();
+
+//    add(imagePanel, BorderLayout.CENTER);
     add(panel);
     add(startBtn);
-    add(pauseBtn);
-//    add(imagePanel, BorderLayout.CENTER);
     setSize(Constants.FRAME_WIDTH, Constants.FRAME_HEIGTH);
     setVisible(true);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setLocationRelativeTo(this);
+
+
+  }
+
+
+  public JMenuBar createMenuBar() {
+    JMenuBar menuBar = new JMenuBar();
+
+    JMenu fileMenu = new JMenu("File");
+    JMenuItem loadMenuItem = new JMenuItem("Load image");
+    JMenuItem detectMenuItem = new JMenuItem("Detect faces");
+    JMenuItem exitMenuItem = new JMenuItem("Exit");
+    fileMenu.add(loadMenuItem);
+    fileMenu.add(detectMenuItem);
+    fileMenu.add(exitMenuItem);
+
+    JMenu aboutMenu = new JMenu("About");
+    JMenu helpMenu = new JMenu("Help");
+
+    menuBar.add(fileMenu);
+    menuBar.add(aboutMenu);
+    menuBar.add(helpMenu);
+
+
+    return menuBar;
   }
 }
