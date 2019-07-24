a2302d87b2769562456a71631397ae9d499099c6
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-07-25 00:00:32 +0200

    app.Page1 done

--- src/app/gui/Gui.java
+++ src/app/gui/Gui.java
@@ -1,4 +1,57 @@
 package app.gui;
 
-public class Gui {
+import javax.swing.*;
+
+import app.page1.Page1;
+import com.recognition.image.constants.Constants;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoCapture;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+
+
+public class Gui extends JFrame {
+  private static final long serialVersionUID = 1L;
+  private JTabbedPane pagesPanel;
+  private JPanel pagePanel1;
+  private JPanel pagePanel2;
+  private JPanel pagePanel3;
+  private JPanel mainPanel1;
+  private JPanel buttonsPannel1;
+  private JButton startButton1;
+  private JButton pauseButton1;
+  private JButton testButton1;
+  private JPanel rootPanel;
+  private JPanel webcamPanel1;
+  private Page1 page1;
+
+  public Gui() {
+    super("Application");
+    setSize(800, 600);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    add(rootPanel);
+    setLocationRelativeTo(this);
+    setResizable(false);
+//    pack();
+    setVisible(true);
+
+    page1 = new Page1(startButton1, pauseButton1, testButton1, webcamPanel1);
+
+
+  }
+
+
+
+
 }
---
+++ src/app/page1/Page1.java
@@ -0,0 +1,120 @@
+package app.page1;
+
+import app.gui.Gui;
+import com.recognition.image.constants.Constants;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoCapture;
+import swing.App;
+import swing.Webcam;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+
+public class Page1 extends JFrame {
+  private static final long serialVersionUID = 1L;
+  private JButton startButton;
+  private JButton pauseButton;
+  private JButton testButton;
+  private JPanel webcamPanel;
+  private Graphics graphics;
+  private boolean status = false;
+  private Color defaultPanelColor;
+
+  public Page1(JButton startButton, JButton pauseButton, JButton testButton, JPanel webcamPanel) {
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    this.testButton = testButton;
+    this.webcamPanel = webcamPanel;
+
+    defaultPanelColor = webcamPanel.getBackground();
+
+    addListenersToPage1();
+  }
+
+  private void addListenersToPage1() {
+    startButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        System.out.println("Start...");
+        webSource = new VideoCapture(0);
+        myThread = new DaemonThread();
+        Thread t = new Thread(myThread);
+        t.setDaemon(true);
+        myThread.runnable = true;
+        t.start();
+        startButton.setEnabled(false);  //start button
+        pauseButton.setEnabled(true);  // stop button
+      }
+    });
+
+    pauseButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        System.out.println("Pause...");
+        myThread.runnable = false;
+        pauseButton.setEnabled(false);
+        startButton.setEnabled(true);
+        webSource.release();
+      }
+    });
+
+    testButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        status = !status;
+        if (status) {
+          webcamPanel.setBackground(Color.ORANGE);
+        } else {
+          webcamPanel.setBackground(defaultPanelColor);
+        }
+        JOptionPane.showMessageDialog(null, "Test...", "Test title", JOptionPane.QUESTION_MESSAGE);
+      }
+    });
+
+  }
+
+
+  // definitions
+  private Page1.DaemonThread myThread = null;
+  int count = 0;
+  VideoCapture webSource = null;
+  Mat frame = new Mat();
+  MatOfByte mem = new MatOfByte();
+
+
+  // class DeamonThread
+  class DaemonThread implements Runnable {
+    protected volatile boolean runnable = false;
+
+    @Override
+    public void run() {
+      synchronized (this) {
+        while (runnable) {
+          if (webSource.grab()) {
+            try {
+              webSource.retrieve(frame);
+              Imgcodecs.imencode(".bmp", frame, mem);
+              BufferedImage buff = ImageIO.read(new ByteArrayInputStream(mem.toArray()));
+              graphics = webcamPanel.getGraphics();
+              if (graphics.drawImage(buff, 0, 0, 640, 480, 0, 0, buff.getWidth(), buff.getHeight(), null))
+                if (runnable == false) {
+                  System.out.println("Going to wait()");
+                  this.wait();
+                }
+            } catch (Exception e) {
+              System.out.println("Error");
+            }
+          }
+        }
+      }
+    }
+  }
+}
