a8ab66c1261356aac26bad3e159d0a60b7a2c082
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-07-24 22:58:14 +0200

    app package created

---
+++ src/app/app/App.java
@@ -0,0 +1,26 @@
+package app.app;
+
+import app.gui.Gui;
+import org.opencv.core.Core;
+
+import javax.swing.*;
+
+public class App {
+  public static void main(String[] args) {
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (ClassNotFoundException | InstantiationException
+            | UnsupportedLookAndFeelException | IllegalAccessException e) {
+      e.printStackTrace();
+    }
+
+    SwingUtilities.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        new Gui();
+      }
+    });
+  }
+}
